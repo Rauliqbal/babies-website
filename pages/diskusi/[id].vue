0279756006e4d7653b2fbf58b6260c0d8fb569b2
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2024-05-22 22:04:24 +0700

    [FIX] bugs

--- pages/diskusi/[id].vue
+++ pages/diskusi/[id].vue
@@ -16,20 +16,28 @@ useHead({
 </script>
 <template>
   <main>
-    <img class="w-full blur-md h-[28rem]" src="/images/background.png" alt="" />
+    <img
+      class="w-full blur-md h-96 md:h-[28rem]"
+      src="/images/background.png"
+      alt=""
+    />
     <section class="container relative">
-      <header class="absolute z-10 -top-[30rem] inset-x-0">
+      <header
+        class="absolute z-10 -top-[25rem] md:-top-[30rem] inset-x-0 px-4 md:px-0"
+      >
         <div>
           <img
-            class="rounded-2xl z-50 h-80 w-full object-cover"
+            class="rounded-2xl z-50 h-64 md:h-80 w-full object-cover"
             src="/images/background.png"
             alt=""
           />
 
-          <h2 class="text-4xl text-white font-bold mt-4">
+          <h2 class="text-3xl md:text-4xl text-white font-bold mt-4">
             {{ data.article.title }}
           </h2>
-          <p class="desc text-white font-semibold">{{ data.article.desc }}</p>
+          <p class="desc text-white font-semibold mt-2">
+            {{ data.article.desc }}
+          </p>
         </div>
       </header>
 
